11ebbb28efafc06c0947ca7114683a4c6aa7d4f7
Maude <maudeck87@gmail.com>
2025-07-14 17:51:33 +0300

    nouvea

--- inc/connexion.php
+++ inc/connexion.php
@@ -1,13 +1,17 @@
-<?php 
-function dbconnect(){
-    static $connect = null ; 
-    if ( $connect === null){
-        $connect = mysqli_connect('localhost' , 'ETU003960' , 'u78Rnwy' , 'db_s2_ETU003960');
-        if (!$connect){
-            die('Erreur' . mysqli_connect_error());
-        }
-        mysqli_set_charset($connect , 'utf8mb4');
+<?php
+function dbconnect() {
+    $host = 'localhost';
+    $user = 'ETU003960';  // Vérifie bien ce nom d'utilisateur
+    $pass = 'u78R7nwy'; // Remplace avec le bon mot de passe
+    $dbname = 'db_s2_ETU003960';
+
+    $db = mysqli_connect($host, $user, $pass, $dbname);
+
+    if (!$db) {
+        die('Erreur de connexion : ' . mysqli_connect_error());
     }
-    return $connect;
+
+    return $db;
 }
 ?>
+
