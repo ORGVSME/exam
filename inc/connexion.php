763b080d02d5bea1bb8ebe93b4a860527a94f59d
Maude <maudeck87@gmail.com>
2025-07-14 17:48:13 +0300

    nouveau

--- inc/connexion.php
+++ inc/connexion.php
@@ -2,7 +2,7 @@
 function dbconnect(){
     static $connect = null ; 
     if ( $connect === null){
-        $connect = mysqli_connect('localhost' , 'root' , '' , 'gestion_emprunt');
+        $connect = mysqli_connect('localhost' , 'ETU003960' , 'u78Rnwy' , 'db_s2_ETU003960');
         if (!$connect){
             die('Erreur' . mysqli_connect_error());
         }
